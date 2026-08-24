1595bdedc13a4d10c701522a7681ca3442640b37
apathydevil <76252555+apathydevil@users.noreply.github.com>
2026-08-24 07:59:09 +0200

    added put and delete endpoints to update and delete specific tasks respectively

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
 from models import Task, TaskCreate
 
 tasks = [Task(id=1, title = "test")]
@@ -18,3 +19,27 @@ async def write_tasks(task: TaskCreate):
     new_task = Task(id=len(tasks) + 1, title=task.title, done=task.done)
     tasks.append(new_task)
     return new_task
+
+@app.get("/tasks/{task_id}")
+async def get_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    raise HTTPException(status_code=404, detail="No task with matching id")
+
+@app.put("/tasks/{task_id}")
+async def update_task(task_id: int, task: TaskCreate):
+    for existing_task in tasks:
+        if existing_task.id == task_id:
+            existing_task.title = task.title
+            existing_task.done = task.done
+            return existing_task
+    raise HTTPException(status_code=404, detail="No task with matching id")
+
+@app.delete("/tasks/{task_id}")
+async def delete_task(task_id: int):
+    for existing_task in tasks:
+        if existing_task.id == task_id:
+            tasks.remove(existing_task)
+            return {"message": f"task {task_id} deleted"}
+    raise HTTPException(status_code=404, detail="No task with matching id")      
